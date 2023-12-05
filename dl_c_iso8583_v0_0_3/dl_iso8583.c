a0e1930b37255388ef1ed73e6e8c35df447d94a7
Zheng Lin Lei <lin.zheng@f1-connecting.com>
2023-12-05 19:27:32 +0100

    Fixed issue #4

--- dl_c_iso8583_v0_0_3/dl_iso8583.c
+++ dl_c_iso8583_v0_0_3/dl_iso8583.c
@@ -238,7 +238,7 @@ DL_ERR DL_ISO8583_MSG_Unpack ( const DL_ISO8583_HANDLER *iHandler,
 	int        haveBitmap  = 0;
 
 	/* unpack all fields until we've encountered a bitmap field */
-	while ( !err && (curFieldIdx < maxFieldIdx) && (curPtr < endPtr) && !haveBitmap )
+	while ( !err && (curFieldIdx <= maxFieldIdx) && (curPtr < endPtr) && !haveBitmap )
 	{
 		err = _DL_ISO8583_FIELD_Unpack(curFieldIdx,ioMsg,iHandler,&curPtr);
 
@@ -250,7 +250,7 @@ DL_ERR DL_ISO8583_MSG_Unpack ( const DL_ISO8583_HANDLER *iHandler,
 	} /* end-while */
 
 	/* unpack only present fields (if any) after bitmap field */
-	while ( !err && (curFieldIdx < maxFieldIdx) && (curPtr < endPtr) )
+	while ( !err && (curFieldIdx <= maxFieldIdx) && (curPtr < endPtr) )
 	{
 		if ( 0 != ioMsg->field[curFieldIdx].len ) /* present */
 		{
